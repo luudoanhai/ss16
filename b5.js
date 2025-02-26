3b1dc6040cf72c3caa38289918faabfeef773a01
luudoanhai <hluudoan@gmail.com>
2025-02-26 22:16:51 +0700

    Create b5.js

---
+++ b5.js
@@ -0,0 +1,4 @@
+let firstNumber = +prompt("Nhap so thu nhat cua ban");
+let secondNumber = +prompt("Nhap so thu hai cua ban");
+let sum = (firstNumber + secondNumber);
+alert("Tong hai so vua nhap la: " + sum);
