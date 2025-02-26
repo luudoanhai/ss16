17fcda7cd3fb6c56951b9e5073b3e51de2b85e0e
luudoanhai <hluudoan@gmail.com>
2025-02-26 22:18:25 +0700

    Create b8.js

---
+++ b8.js
@@ -0,0 +1,5 @@
+let firstNumber = Number(prompt("Nhập số thứ nhất: "));
+let secondNumber = Number(prompt("Nhập số thứ hai: "));
+let threeNumber = Number(prompt("Nhập số thứ ba: "));
+let maxNumber = Math.max(firstNumber, secondNumber, threeNumber);
+document.write(`Số lớn nhất trong ba số la: ` + maxNumber + `<br>`);
