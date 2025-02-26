ea575c838ff96c0176d7c271d445579127acedd4
luudoanhai <hluudoan@gmail.com>
2025-02-26 22:14:44 +0700

    Create b2.js

---
+++ b2.js
@@ -0,0 +1,9 @@
+let number = 10;
+let price = 20.5;
+let status = true;
+let city = "Hà Nội";
+
+document.write("Biến <b>number</b> kiểu số nguyên, có giá trị là: <b>" + number +"</b><br>")
+document.write("Biến <b>price</b> kiểu số thựcthực, có giá trị là: <b>" + price +"</b><br>")
+document.write("Biến <b>status</b> kiểu logic, có giá trị là: <b>" + status +"</b><br>")
+document.write("Biến <b>city</b> kiểu chuỗi, có giá trị là: <b>" + city +"</b><br>")
